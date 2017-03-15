c1d53efe77dd81120213a7f427ecc9eb6f304e08
Ksenia Mitina <k.mitina@corp.mail.ru>
2017-03-15 20:56:32 +0300

    HT4 - added filter by data from store

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -6,41 +6,77 @@ import accrdion from '../../decorators/accordion'
 import './style.css'
 
 class ArticleList extends Component {
-    render() {
-        const {articles, toggleOpenItem, isItemOpened} = this.props
+	render() {
+		let {articles} = this.props
+		const {toggleOpenItem, isItemOpened, filters} = this.props
+		const {selected, from, to} = filters
 
-        const articleComponents = articles.map(article => <li key={article.id}>
-            <Article article={article}
-                     isOpen={isItemOpened(article.id)}
-                     toggleOpen={toggleOpenItem(article.id)}
-            />
-        </li>)
+		const from_d = new Date(from).getTime()
+		const to_d = new Date(to).getTime()
 
-        return (
-            <CSSTransition component="ul"
-                           transitionName="article-list"
-                           transitionAppear={true}
-                           transitionAppearTimeout={100}
-                           transitionEnterTimeout={500}
-                           transitionLeaveTimeout={300}
-            >
-                {articleComponents}
-            </CSSTransition>
-        )
-    }
+
+		if (selected && selected.length || from || to) {
+			articles = articles.filter(article => {
+				const d = new Date(article.date).getTime();
+				let f_sel = false
+				let f_d = false
+
+				if (selected && selected.length) {
+					f_sel = selected.findIndex(el => el.value === article.id) !== -1
+				}
+				else {
+					f_sel = true
+				}
+
+				if (from || to) {
+					if (!to) {
+						f_d = (d === from_d)
+					}
+					else {
+						f_d = (from_d < d) && (d < to_d)
+					}
+				}
+				else {
+					f_d = true
+				}
+
+				return f_sel && f_d
+			})
+		}
+
+		const articleComponents = articles.map(article => <li key={article.id}>
+			<Article article={article}
+					 isOpen={isItemOpened(article.id)}
+					 toggleOpen={toggleOpenItem(article.id)}
+			/>
+		</li>)
+
+		return (
+				<CSSTransition component="ul"
+							   transitionName="article-list"
+							   transitionAppear={true}
+							   transitionAppearTimeout={100}
+							   transitionEnterTimeout={500}
+							   transitionLeaveTimeout={300}
+				>
+					{articleComponents}
+				</CSSTransition>
+		)
+	}
 }
 
 const mapStateToProps = state => {
-    console.log('---', 'connect, state = ', state)
-    return {
-        articles: state.articles
-    }
+	console.log('---', 'connect, state = ', state)
+	return {
+		articles: state.articles,
+		filters: state.filters
+	}
 }
 
 export default connect(mapStateToProps)(accrdion(ArticleList))
 
 ArticleList.propTypes = {
-    articles: PropTypes.array.isRequired,
-    isItemOpened: PropTypes.func.isRequired,
-    toggleOpenItem: PropTypes.func.isRequired
+	articles: PropTypes.array.isRequired,
+	isItemOpened: PropTypes.func.isRequired,
+	toggleOpenItem: PropTypes.func.isRequired
 }
