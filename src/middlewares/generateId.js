d745ab28a2692f4370538b3ef6308bcd0d920c6f
Roman <kaboom.ua@gmail.com>
2017-03-22 23:00:52 +0200

    Update generateId.js

--- src/middlewares/generateId.js
+++ src/middlewares/generateId.js
@@ -2,7 +2,10 @@ import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
     console.log('---', 'before: ', store.getState())
+	    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
 	if (action.type === ADD_COMMENT) {
+		//зачем window?
+	        //завязка на length - очень плохая логика, что если удалят коммент?
 		const id = window.store.getState().comments.length + 1;
 		next({ ...action, payload: { ...action.payload, id } })
 	} else {
